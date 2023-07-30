c6bab2867c339cbdb2cfe0015172cd72075e20bc
EtherShade <135162361+EtherShade@users.noreply.github.com>
2023-07-30 13:59:02 -0400

    First Change

--- src/main/java/net/ethershadeyt/tutorialmod/TutorialMod.java
+++ src/main/java/net/ethershadeyt/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.ethershadeyt.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ethershadeyt.tutorialmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +27,8 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
